86356af42c96776c30c189bec2361c30c627e971
Osman Shakib <osmanshakib@gmail.com>
2021-11-23 03:59:05 +0000

    commit

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected){
+        console.log(`These two are the same: ${actual} vs ${expected}`)
+    } else {
+        console.log(`These two are not the same: ${actual} vs ${expected}`);
+    }
+
+};
